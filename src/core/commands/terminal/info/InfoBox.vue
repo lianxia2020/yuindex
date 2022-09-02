fcc72f4730b5f4bdcbe5453d3774c57bfb7246ee
lianxia2020 <83966658+lianxia2020@users.noreply.github.com>
2022-09-03 04:17:10 +0800

    bug

--- src/core/commands/terminal/info/InfoBox.vue
+++ src/core/commands/terminal/info/InfoBox.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>关于本站:Lianxia</br>
-  <a href="https://lianxia.icu/">Lianxia</a>
-  </div>
-  <div></div>
-  <div></div>
-  <div></div>
+  
 </template>
 
 <script setup lang="ts">
