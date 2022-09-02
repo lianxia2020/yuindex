3ba20eac4e39a165160ea52d1135b51f093e70a8
lianxia2020 <83966658+lianxia2020@users.noreply.github.com>
2022-09-02 16:09:09 +0800

    geng地址 替换

--- src/core/commands/terminal/info/InfoBox.vue
+++ src/core/commands/terminal/info/InfoBox.vue
@@ -1,20 +1,10 @@
 <template>
-  <div>关于本站：Lianxia</br>
+  <div>关于本站:Lianxia</br>
   <a href="https://lianxia.icu/">Lianxia</a>
   </div>
-  <div>
-  https://github.com/lianxia2020/yuindex
-    <a href="https://github.com/liyupi/yuindex" target="_blank">
-      代码开源，欢迎 star 和贡献~
-    </a>
-  </div>
   <div></div>
-  <div>
-    作者：
-    <a href="https://docs.qq.com/doc/DUFFRVWladXVjeUxW" target="_blank">
-      程序员鱼皮
-    </a>
-  </div>
+  <div></div>
+  <div></div>
 </template>
 
 <script setup lang="ts">
